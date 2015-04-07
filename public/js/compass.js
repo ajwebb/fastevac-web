1b51c4e0d512ce3830e4a5859a63252b12c04b32
Adam Webb <webbjadam@gmail.com>
2015-04-06 18:08:47 -0700

    reworked compass again to always point north

--- public/js/compass.js
+++ public/js/compass.js
@@ -10,10 +10,17 @@
             window.addEventListener("deviceorientation", function (e) {
                 var heading = null;
                 if (e.alpha !== null) {
-                    heading = compassHeading(e.alpha, e.beta, e.gamma);
-                    //console.log('compass heading: '+ heading);
-                };
-                rotate(heading);
+                    //heading = compassHeading(e.alpha, e.beta, e.gamma);
+                    if (e.webkitCompassHeading) {
+                        heading = e.webkitCompassHeading
+                    }
+                    else {
+                        heading = e.alpha;
+                    }
+
+                    console.log('compass heading: '+ heading);
+                    rotate(360 - heading);
+                }
             }, false);
         };
 
@@ -28,42 +35,6 @@
         enableHighAccuracy: true, 
         maximumAge        : 30000, 
         timeout           : 27000
-    };
-
-    function compassHeading(alpha, beta, gamma) {
-
-        // Convert degrees to radians
-        var alphaRad = alpha * (Math.PI / 180);
-        var betaRad = beta * (Math.PI / 180);
-        var gammaRad = gamma * (Math.PI / 180);
-
-        // Calculate equation components
-        var cA = Math.cos(alphaRad);
-        var sA = Math.sin(alphaRad);
-        var cB = Math.cos(betaRad);
-        var sB = Math.sin(betaRad);
-        var cG = Math.cos(gammaRad);
-        var sG = Math.sin(gammaRad);
-
-        // Calculate A, B, C rotation components
-        var rA = - cA * sG - sA * sB * cG;
-        var rB = - sA * sG + cA * sB * cG;
-        var rC = - cB * cG;
-
-        // Calculate compass heading
-        var compassHeading = Math.atan(rA / rB);
-
-        // Convert from half unit circle to whole unit circle
-        if(rB < 0) {
-            compassHeading += Math.PI;
-        }else if(rA < 0) {
-            compassHeading += 2 * Math.PI;
-        }
-
-        // Convert radians to degrees
-        compassHeading *= 180 / Math.PI;
-
-        return compassHeading;
     };
 
     function geoError() {
